1f001e4920e1a992604094371fa41b4faf075bac
John Smith <xfgryujk@126.com>
2017-09-18 00:05:45 +0800

    添加程序运行时注入DLL

--- DllInjection/RemoteThread/RemoteThread.cpp
+++ DllInjection/RemoteThread/RemoteThread.cpp
@@ -71,6 +71,69 @@ HMODULE InjectDll(HANDLE process, LPCTSTR dllPath)
 	return (HMODULE)remoteModule;
 }
 
+// 程序运行时注入DLL，返回模块句柄（64位程序只能返回低32位）
+HMODULE InjectDll(LPTSTR commandLine, LPCTSTR dllPath, DWORD* pid, HANDLE* process)
+{
+	TCHAR* commandLineCopy = new TCHAR[32768]; // CreateProcess可能修改这个
+	_tcscpy_s(commandLineCopy, 32768, commandLine);
+	int cdSize = _tcsrchr(commandLine, _T('\\')) - commandLine + 1;
+	TCHAR* cd = new TCHAR[cdSize];
+	_tcsnccpy_s(cd, cdSize, commandLine, cdSize - 1);
+	// 创建进程并暂停
+	STARTUPINFO startInfo = {};
+	PROCESS_INFORMATION processInfo = {};
+	if (!CreateProcess(NULL, commandLineCopy, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, cd, &startInfo, &processInfo))
+	{
+		delete commandLineCopy;
+		delete cd;
+		return 0;
+	}
+	delete commandLineCopy;
+	delete cd;
+
+	*pid = processInfo.dwProcessId;
+	*process = processInfo.hProcess;
+
+	DWORD dllPathSize = ((DWORD)_tcslen(dllPath) + 1) * sizeof(TCHAR);
+
+	// 申请内存用来存放DLL路径
+	void* remoteMemory = VirtualAllocEx(processInfo.hProcess, NULL, dllPathSize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (remoteMemory == NULL)
+	{
+		printf("申请内存失败，错误代码：%u\n", GetLastError());
+		return 0;
+	}
+
+	// 写入DLL路径
+	if (!WriteProcessMemory(processInfo.hProcess, remoteMemory, dllPath, dllPathSize, NULL))
+	{
+		printf("写入内存失败，错误代码：%u\n", GetLastError());
+		return 0;
+	}
+
+	// 创建远线程调用LoadLibrary
+	HANDLE remoteThread = CreateRemoteThread(processInfo.hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, remoteMemory, 0, NULL);
+	if (remoteThread == NULL)
+	{
+		printf("创建远线程失败，错误代码：%u\n", GetLastError());
+		return NULL;
+	}
+	// 等待远线程结束
+	WaitForSingleObject(remoteThread, INFINITE);
+	// 取DLL在目标进程的句柄
+	DWORD remoteModule;
+	GetExitCodeThread(remoteThread, &remoteModule);
+
+	// 恢复线程
+	ResumeThread(processInfo.hThread);
+
+	// 释放
+	CloseHandle(remoteThread);
+	VirtualFreeEx(processInfo.hProcess, remoteMemory, dllPathSize, MEM_DECOMMIT);
+
+	return (HMODULE)remoteModule;
+}
+
 // 卸载DLL
 BOOL FreeRemoteDll(HANDLE process, HMODULE remoteModule)
 {
@@ -141,6 +204,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	// 注入DLL
 	HMODULE remoteModule = InjectDll(process, dllPath);
+	//HMODULE remoteModule = InjectDll(_T("C:\\Windows\\System32\\Taskmgr.exe"), dllPath, &pid, &process);
 	if (remoteModule == NULL)
 	{
 		CloseHandle(process);
